e23bc02befa61ab59ca759d138693785b44fe748
Pradeep <sahup2504@gmail.com>
2025-02-01 23:06:57 +0530

    all function are add to accourding to keys and name , it work properly 250419971

--- List_element.py
+++ List_element.py
@@ -1,7 +1,7 @@
 list = [2,4,5,6,7,8,9,9,1,3,4,5,6,7]
 choose = 0
 while True:
-    print("List  all element are excuted 'List' ", list)
+    print("\n\nList  all element are excuted 'List' ", list)
     print(" 1.  Append an element")
     print(" 2.  Insert the element in the position")
     print(" 3.  Append a list to the given list ")
@@ -20,38 +20,57 @@ while True:
         print(list)
         print("\n")
         continue
+    
     elif choose == 2:
         element = int(input("Enter the element to be append: " ))
         position = int(input("enter the position to add element: "))
         list.insert(position,element)
         print("element append in the  given position  in succesfully\n")
+    
     elif choose == 3:
         newList = int(input("Enter the element to be append: " ))
         list.extend(newList)
         print("new list add on existing list \n")
+    
     elif choose == 4:
-         i = int(input("enter the any of the position on the list :"))
-         if i < len(list):
+        i = int(input("enter the any of the position on the list :"))
+        if i < len(list):
             newListElement = int(input("enter the new element  to modify possition of the list :"))
             list[i] = newListElement
             oldelement = list[i]
             print("the element ", oldelement,"has been mofield")
+    
     elif choose == 5:
-
-
+        possition = int(input("enter the list of possition :"))
+        if possition < len(list):
+             new_list = list.pop(possition)
+             list_delet = list[new_list]
+             print("the element has" ,list_delet,"deleted succesfully")
+             print("new list is := ", list)
+        else :
+            print("given element position is gratter than to length of\n\n")
+    
     elif choose == 6:
-
-
+        element_value = int(input("enter the inlcuding element value := "))
+        if element_value in list:
+            list.remove(element_value)
+            print("the given valur",element_value, " succesfully remove ")
+            print("new list is := ", list)
+        else:
+            print("enter the element value",element_value,"not in list ")
+    
     elif choose == 7:
+        print(list.sort())
+        print("the list has been sorted succesfully Accending order ")
     
-
     elif choose == 8:
+        list.sort(reversed == True)
+        print("the list has been sorted succesfully desending order ")
     
-
     elif choose == 9:
-
-
+         print("the list is : =",list)
+    
     elif choose == 10:
-        break
+         break
 
 print("exite with while loop")
